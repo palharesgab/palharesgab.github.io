c8904408d8aabbac1df70d7f11bebb026c774074
Gabriel Palhares <pizzanigabriel@gmail.com>
2022-12-27 00:20:34 -0300

    commit2

---
+++ script.js
@@ -0,0 +1,25 @@
+function sobreMim()
+{
+    window.scrollTo(0, 1100);
+}
+
+function curriculo()
+{
+    window.scrollTo(0, 1350);
+}
+
+function projetos()
+{
+    window.scrollTo(0, 1800);
+}
+
+function educacao()
+{
+    window.scrollTo(0, 1800);
+}
+
+function skills()
+{
+    window.scrollTo(0, 1800);
+}
+
