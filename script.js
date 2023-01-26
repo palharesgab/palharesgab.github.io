f0a8ad85a848dad59dbe24ef4487adde0c1efe4b
Gabriel Palhares <pizzanigabriel@gmail.com>
2023-01-25 21:42:05 -0300

    Refatorado portfolio

--- script.js
+++ script.js
@@ -1,20 +1,2 @@
-function sobreMim()
-{
-    window.scrollTo(0, 1100);
-}
 
-function curriculo()
-{
-    window.scrollTo(0, 1350);
-}
-
-function educacao()
-{
-    window.scrollTo(0, 1800);
-}
-
-function skills()
-{
-    window.scrollTo(0, 1800);
-}
 
